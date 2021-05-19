619777ed930177053b28a80c2a577b73ee23ff1c
Kamil Kowalczyk <kkowalczyk@ifxpayments.com>
2021-05-19 09:17:41 +0200

    Add CountCommand.php and change console.php

--- console.php
+++ console.php
@@ -4,8 +4,9 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
+use App\Command\CountCommand;
 
 $app = new Application();
-
+$app->add(new CountCommand());
 
 $app->run();
